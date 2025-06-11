22e5fe0f3dbd66329e0a2e9de4d84a1e38c77fe0
lanaal <svetlana.alyabjeva@gmail.com>
2025-06-11 18:13:59 +0300

    Lombok + login tests

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class BasePage {
     static WebDriver driver;
@@ -14,5 +15,13 @@ public class BasePage {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean isTextElementPresent(WebElement element, String text){
+        return element.getText().contains(text);
+    }
+
+    public boolean isElementPresent(WebElement element){
+        return element.isDisplayed();
     }
 }
--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,11 +1,25 @@
 package ui_tests;
 
+import dto.UserLombok;
 import manager.ApplicationManager;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
+import static utils.RandomUtils.*;
 
 public class LoginTests extends ApplicationManager {
+
+    HomePage homePage;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void goToLoginPage(){
+        homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        loginPage = new LoginPage(getDriver());
+    }
 
     @Test
     public void loginPositiveTest(){
@@ -13,5 +27,35 @@ public class LoginTests extends ApplicationManager {
         homePage.clickBtnLoginHeader();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginForm("email1@mail.ru", "ema31il@Mail.ru");
+    }
+
+    @Test
+    public void loginPositiveTestLombok(){
+        UserLombok user = UserLombok.builder()
+                .username("email1@mail.ru")
+                .password("ema31il@Mail.ru")
+                    .build();
+        loginPage.typeLoginForm(user.getUsername(), user.getPassword());
+        Assert.assertTrue(loginPage.validatePopUpMessage("Logged in success"), "test upal");
+    }
+
+    @Test
+    public void loginNegativeTest_unregUser(){
+        UserLombok user = UserLombok.builder()
+                .username(generateEmail(10))
+                .password("ema31il@Mail.ru")
+                .build();
+        loginPage.typeLoginForm(user.getUsername(), user.getPassword());
+        Assert.assertTrue(loginPage.validatePopUpMessage("Login or Password incorrect"), "test upal");
+    }
+
+    @Test
+    public void loginNegativeTest_emptyPassword(){
+        UserLombok user = UserLombok.builder()
+                .username("email1@mail.ru")
+                .password("")
+                .build();
+        loginPage.typeLoginForm(user.getUsername(), user.getPassword());
+        Assert.assertTrue(loginPage.validateMessageErrorPassword(), "loginNegativeTest_emptyPassword");
     }
 }
